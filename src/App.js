2f9f202156aff6655d94c00282a4778c97ec3f51
Amit-g12 <amitkumargang123@gmail.com>
2022-10-10 16:44:13 +0000

    react-day-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { useEffect, useState } from 'react';
+import UseMemo from './UseMemo';
+import UseReducer from './UseReducer';
 
 function App() {
+
+  const[player,setplayer]= useState('sachin')
+  const[numbers,setnumbers]= useState([1,3,5,7,9])
+  const[language,setlanguage]= useState('java')
+
+
+  useEffect(()=>{
+
+    setlanguage('JavaScript')
+
+  },[])
+
+  function changeplayer(){
+
+    setplayer('Dhoni')
+    setnumbers([2,4,6,8,10])
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Hooks</h1>
+      <h1>The decleared player is {player}</h1>
+      <h1>the decleared numbers is{numbers}</h1>
+      <button onClick={changeplayer}>Change Player</button>
+      
+      <h1>the decleared language is {language}</h1>
+
+      <Home/>
+      <UseMemo/>
+      <UseReducer/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+
+function Home(){
+
+    const[color, setcolor]= useState('red')
+    const[bgcolor,setbgcolor]= useState('yellow')
+
+    function changestyling(){
+        setcolor('green')
+        setbgcolor('blue')
+    }
+
+    return <div>
+        <h1>manipulating css using hooks</h1>
+
+        <h1 style={{color:color}}>Ronaldo</h1>
+        <h1 style={{backgroundColor:bgcolor}}>Football</h1>
+
+
+        <button onClick={changestyling}>Change Styling</button>
+    </div>
+}
+export default Home
